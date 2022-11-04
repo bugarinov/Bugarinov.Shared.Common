f409583a94981918de4268b87d560b0923ab100d
Yuliy Bugarin <bugarindev@yahoo.com>
2022-11-04 13:07:08 +0800

    feat: added common base presenter class

---
+++ ViewModels/BasePresenter.cs
@@ -0,0 +1,20 @@
+﻿using Bugarinov.Shared.Common.Core;
+using Bugarinov.Shared.Common.Factory;
+using Bugarinov.Shared.Common.Services;
+
+namespace Bugarinov.Shared.Common.ViewModels
+{
+    public class BasePresenter : ObservableObject
+    {
+        protected readonly INavigationService NavigationService;
+        protected readonly IPresenterFactory PresenterFactory;
+
+        public BasePresenter(INavigationService navigationService, IPresenterFactory presenterFactory)
+        {
+            NavigationService = navigationService;
+            PresenterFactory = presenterFactory;
+        }
+
+        public virtual void Unload() { }
+    }
+}
