10986dd8d7d7f0e5f1db18c0142e2793fe4d360c
Yuliy Bugarin <bugarindev@yahoo.com>
2022-11-04 13:06:50 +0800

    feat: added common navigation class

---
+++ Services/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using Bugarinov.Shared.Common.ViewModels;
+using System;
+
+namespace Bugarinov.Shared.Common.Services
+{
+    public interface INavigationService
+    {
+        BasePresenter Content { get; set; }
+        void Navigate(BasePresenter presenter);
+        void DialogNavigate(BasePresenter presenter, bool isDialogMode = true);
+        bool ContentIs(Type type);
+    }
+}
